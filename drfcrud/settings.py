7caa9257188a70239747d5c224dd8d67755a92e3
sunflower <rasp17698@gmail.com>
2022-09-19 17:05:24 -0500

    add: latest changes to deployment

--- drfcrud/settings.py
+++ drfcrud/settings.py
@@ -21,11 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0s=809=p^3pejv-1$0=c8jqdv6q8vz&z8uqp+pxb5or9p+git$"
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
