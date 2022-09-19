9d83e8979721db8893a0ba076d86df9f328f353b
sunflower <rasp17698@gmail.com>
2022-09-19 15:59:00 -0500

    first commit

---
+++ projects/api.py
@@ -0,0 +1,9 @@
+import projects
+from .models import Project
+from rest_framework import viewsets, permissions
+from .serializers import ProjectSerializer
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ProjectSerializer
---
+++ projects/serializers.py
@@ -0,0 +1,10 @@
+from msilib.schema import Class
+from rest_framework import serializers
+from .models import Project
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('id', 'title', 'description', 'technology', 'created_at')
+        read_only_fields = ('created_at',) #campo de solo lectura para que no se pueda modificar el campo
+
